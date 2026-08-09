93e441c037f94144b5ebf41cc6fb562d8dc2d271
lqdxt <nothingidk793@gmail.com>
2026-08-09 17:10:26 -0600

    Add script to load Lua from external API
    Add script to load a Lua script from a deprecated API link.

---
+++ VH.script.lua
@@ -0,0 +1,2 @@
+-- git has been deprecated but ill add the link anyways
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b3eb88f529415bf4ba11197f32d533a0c8dc6dcb906154978e9f420de39f345e/download"))()
