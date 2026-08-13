5d6abe75addda91a27aa5fd90828afbae228909c
lqdxt <nothingidk793@gmail.com>
2026-08-12 22:18:04 -0600

    Implement notification function for script warnings
    Added a notification function to alert users about potential script issues.

--- VH.script.lua
+++ VH.script.lua
@@ -1,2 +1,12 @@
 -- git has been deprecated but ill add the link anyways
+SendNotif = function(title, text, duration)
+  pcall(function()
+      game:GetService("StarterGui"):SetCore("SendNotification", {
+          Title = title
+          Text = text
+          Duration = duration
+        })
+    end)
+end
+SendNotif("warning", "script may timeout/crash because there's an issue with luraph", 5)
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b3eb88f529415bf4ba11197f32d533a0c8dc6dcb906154978e9f420de39f345e/download"))()
